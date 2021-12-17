d5d47fb2c4969771c956c0bb365f1b3ff6d19eeb
Italo Evan <igbehh@gmail.com>
2021-12-17 20:16:27 -0300

    BEGIN

--- lib/custom_header.dart
+++ lib/custom_header.dart
@@ -1,7 +1,25 @@
 library custom_header;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class CustomHeader extends StatelessWidget {
+  final double? height;
+  final String? title;
+
+  const CustomHeader({Key? key, this.height, this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: height ?? 90,
+      child: Padding(
+        padding: EdgeInsets.all(10),
+        child: Row(
+          children: [
+            title != null ? Text(title!) : const SizedBox(),
+          ],
+        ),
+      ),
+    );
+  }
 }
